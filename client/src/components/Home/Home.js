095ba7725e73b3960709ebc2d1a803e70afc40df
Finlie <dfinley2917@gmail.com>
2018-08-16 12:45:32 -0400

    testing card layout on landing page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from '@material-ui/core';
+import { Button, Grid, Paper, Card, CardActions, CardContent, CardMedia, Typography } from '@material-ui/core';
 import './index.css';
 
 export default class Home extends Component {
@@ -17,6 +17,25 @@ export default class Home extends Component {
                 </div> {/* Top End */}
                 <div className='Home_Content'> {/* Main Content*/}
                     <h1>What is a Knowledge Rocket?</h1>
+                    <Grid container>
+                        <Card>
+                            <CardMedia
+                                className='(classes.media)'
+                                image='./client/public/img/rocket-blast-clipart-1.jpg'
+                                title='Blast Off In To Greatness!'
+                            />
+                            <CardContent>
+                                <Typography component="p">
+                                    Using Knowledge Rockets will increase your student's comprehension while making your job as a teacher easier!
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button variant="contained" color="primary">
+                                    Find Out More
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
                 </div> {/* Main Content End */}
                 <div className='Home_Bottom'> {/* Bottom */}
                     <footer>
