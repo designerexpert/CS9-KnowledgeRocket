ee5e803aeeed0f5185809ed576ebe259333f30a4
Finlie <dfinley2917@gmail.com>
2018-08-21 10:46:09 -0400

    footer updates

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,128 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import Button from '@material-ui/core/Button';
-import './index.css';
-import Styled from 'styled-components';
-
-const HomeCTA = Styled.div`
-    background: url('./img/HomePage_CTA_04.svg') no-repeat center center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    min-height: 880px;
-    &:hover {
-        cursor: pointer;
-    }
-    @media(max-width: 800px) {
-        width: 100vw;
-        min-height: 100vh;
-    }
-`;
-
-const HomeContainer = Styled.div`
-    font-family: 'Roboto', sans-serif;
-`;
-
-const HomeCtaHeaderLeft = Styled.h1`
-    font-size: 4rem;
-    font-weight: 900;
-    color: #005B97;
-    min-width: 300px;
-    width: 35vw;
-    text-align: center;
-    padding-top: 60vh;
-    @media(max-width: 1400px){
-        font-size: 2.8rem;
-    }
-    @media(max-width: 1200px){
-        font-size: 2.3rem;
-    }
-`;
-const HomeCtaHeaderRight = Styled.h1`
-    font-size: 4rem;
-    font-weight: 900;
-    color: #005B97;
-    min-width: 300px;
-    width: 35vw;
-    text-align: center;
-    padding-top: 60vh;
-    @media(max-width: 1400px){
-        font-size: 2.8rem;
-    }
-    @media(max-width: 1200px){
-        font-size: 2.3rem;
-    }
-`;
-
-const HomeCtaHeaderRight2 = Styled(HomeCtaHeaderRight)`
-    padding-top: 0;
-`;
-
-const MidContainer = Styled.div`
-    font-family: 'Roboto', sans-serif;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`;
-
-const HomeCtaHeaderWrapper = Styled.div`
-    width: 100%;
-    display:none;
-    @media(min-width: 700px) {
-        margin: 0 auto;
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        align-items: flex-end;
-        justify-content: space-between;
-        width: 100%;
-        max-width: 1920px;
-    }
-`;
-
-const HomeHeader = Styled.h2`
-    font-size: 4rem;
-    font-weight: 900;
-    color: #005B97;
-    min-width: 300px;
-    max-width: 800px;
-    text-align: center;
-    margin: 3rem 0;
-`;
-
-const MidSectionWrapper = Styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
-    width: 100%;
-`;
-
-const MidSection = Styled.div`
-    display: flex;
-    flex-direction: column;
-    min-width: 300px;
-    max-width: 30%;
-    background-color: #DFDFDF;
-    border-radius: .5rem;
-    border: 1px solid #005B97;
-    margin: 1rem;
-    box-shadow: 0px 0px 12px 0px rgba(0,0,0,0.5)
-`;
-
-const MidImg = Styled.img`
-    margin: 1rem;
-    height: 200px;
-    background-color: #FEFEFE;
-    border-radius: .3rem;
-`;
-
-const MidSectionText = Styled.p`
-    margin: 1rem;
-    font-size: 1.5rem;
-    text-align: justify;
-`;
-=======
 import Logo from '../../assets/Logo_BW_SML.svg';
 import {
     HomeContainer,
@@ -140,7 +16,6 @@ import {
     MidSectionText,
     FooterSection,
 } from './HomeStyled';
->>>>>>> master
 
 export default class Home extends Component {
     handleAuthRedirect = e => {
@@ -158,95 +33,6 @@ export default class Home extends Component {
     render() {
         return (
             <HomeContainer>
-<<<<<<< HEAD
-                <HomeCTA onClick={this.handleAuthRedirect}>
-                    <div className="Home_ButtonDiv">
-                        <Button
-                            className="Home_Button"
-                            color="primary"
-                            onClick={this.handleAuthRedirect}
-                        >
-                            Sign Up
-                        </Button>
-                        <Button
-                            className="Home_Button"
-                            color="primary"
-                            onClick={this.handleAuthRedirect}
-                        >
-                            Sign In
-                        </Button>
-                    </div>
-                    <HomeCtaHeaderWrapper>
-                        <div>
-                            <HomeCtaHeaderLeft>KNOWLEDGE</HomeCtaHeaderLeft>
-                            <HomeCtaHeaderRight2>ROCKET</HomeCtaHeaderRight2>
-                        </div>
-                        <div>
-                            <HomeCtaHeaderRight>LAUNCH YOUR</HomeCtaHeaderRight>
-                            <HomeCtaHeaderRight2>LEARNING</HomeCtaHeaderRight2>
-                        </div>
-                    </HomeCtaHeaderWrapper>
-                </HomeCTA>
-                <MidContainer>
-                    <HomeHeader>What is a Knowledge Rocket?</HomeHeader>
-                    <MidSectionWrapper>
-                        <MidSection>
-                            <MidImg />
-                            <MidSectionText>
-                                Text for mid section here should not be very length but should
-                                contain enough information about this section.Text for mid section
-                                here should not be very length but should contain enough information
-                                about this section.
-                            </MidSectionText>
-                        </MidSection>
-                        <MidSection>
-                            <MidImg />
-                            <MidSectionText>
-                                Text for mid section here should not be very length but should
-                                contain enough information about this section.Text for mid section
-                                here should not be very length but should contain enough information
-                                about this section.Text for mid section here should not be very
-                                length but should contain enough information about this section.
-                            </MidSectionText>
-                        </MidSection>
-                        <MidSection>
-                            <MidImg />
-                            <MidSectionText>
-                                Text for mid section here should not be very length but should
-                                contain enough information about this section.Text for mid section
-                                here should not be very length but should contain enough information
-                                about this section.
-                            </MidSectionText>
-                        </MidSection>
-                    </MidSectionWrapper>
-                </MidContainer>
-                <BottomContainer>
-                    <BottomSectionWrapper>
-                        <BottomSectionLeft>
-                            <link>
-                                <a href={'aboutusURL()'}>About Us</a>
-                            </link>
-                            <link>
-                                <a href={pricingURL()}>Pricing</a>
-                            </link>
-                            <link>
-                                <a href={privacyURL()}>Privacy</a>
-                            </link>
-                            <link>
-                                <a href={accessabilityURL()}>Accessability</a>
-                            </link>
-                        </BottomSectionLeft>
-                        <BottomSectionRight>
-                        <link>
-                            <a href={githubUrl()}>GitHub</a>
-                        </link>
-                        <link>
-                            <a href={twitterUrl()}>Twitter</a>
-                        </link>
-                        </BottomSectionRight>
-                    </BottomSectionWrapper>
-                </BottomContainer>
-=======
                 <HomeCTA>
                     <HomeNav>
                         <div>
@@ -306,8 +92,13 @@ export default class Home extends Component {
                     <NavButton onClick={this.handleContact}>CONTACT US</NavButton>
                     <NavButton onClick={this.handleAuthRedirect}>SIGN UP</NavButton>
                     <NavButton onClick={this.handleAuthRedirect}>SIGN IN</NavButton>
+                    <p>Follow us on 
+                        <a href="https://twitter.com/KnowledegeR" 
+                        title="Knowledge Rocket on Twitter">TWITTER</a> and 
+                        <a href="https://www.facebook.com/Knowledge-Rocket-464965013986045"
+                            title="Knowledge Rocket on Facebook">FACEBOOK</a>
+                    </p>
                 </FooterSection>
->>>>>>> master
             </HomeContainer>
         );
     }
